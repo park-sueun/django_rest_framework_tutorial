9f516954256fda18440f56f464e4d323a496d442
Park, Su Eun <park_sueun@naver.com>
2024-05-07 23:18:34 +0900

    added posts url in routers

--- drftutorial/instagram/urls.py
+++ drftutorial/instagram/urls.py
@@ -1,3 +1,10 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register("posts", views.PostViewSet)
+
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
