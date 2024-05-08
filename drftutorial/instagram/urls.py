d6848ad9c36136dc58c2e04382bbad9b266e91d6
Park, Su Eun <park_sueun@naver.com>
2024-05-08 15:28:44 +0900

    PublicPostListViewSet 사용해보기

--- drftutorial/instagram/urls.py
+++ drftutorial/instagram/urls.py
@@ -7,4 +7,5 @@ router.register("posts", views.PostViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('public/', views.PublicPostListViewSet.as_view()),
 ]
--- drftutorial/instagram/views.py
+++ drftutorial/instagram/views.py
@@ -1,8 +1,14 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .models import Post
 from .serializers import PostSerializer
 
 # Create your views here.
+
+class PublicPostListViewSet(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+    
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
