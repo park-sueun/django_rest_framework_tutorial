0bd1f6ccc9ad87e96d608e6178f9ab2f0bf536d2
Park, Su Eun <park_sueun@naver.com>
2024-05-08 15:31:35 +0900

    author field 추가 및 마이그레이션

--- drftutorial/instagram/models.py
+++ drftutorial/instagram/models.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     message = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
