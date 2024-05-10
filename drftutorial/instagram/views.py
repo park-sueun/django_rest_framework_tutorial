95d998a5a49ca4f3cca13c4d2e35b973762505a6
Park, Su Eun <park_sueun@naver.com>
2024-05-10 23:36:48 +0900

    APIView TODO 추가

--- drftutorial/instagram/views.py
+++ drftutorial/instagram/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, generics
+from rest_framework.views import APIView
 from .models import Post
 from .serializers import PostSerializer
 
@@ -13,3 +14,7 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
+# TODO: APIView로 만들어보기!
+class PostView(APIView):
+    def get(self, request):
+        pass
