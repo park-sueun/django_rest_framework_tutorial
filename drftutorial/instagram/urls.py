bf47dd16744540b3ae05b4ec17a1f6cf4eec6d0c
Park, Su Eun <park_sueun@naver.com>
2024-05-12 22:38:57 +0900

    DRF의 ModelViewSet 활용하기

--- drftutorial/instagram/urls.py
+++ drftutorial/instagram/urls.py
@@ -2,7 +2,11 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register('posts', views.PostViewSet)
+
 urlpatterns = [
-    path('posts/', views.PublicPostListView.as_view(), name='PublicPostListView'),
-    path('posts/<int:pk>/', views.PostView.as_view(), name='PostView'),
+    # path('posts/', views.PublicPostListView.as_view(), name='PublicPostListView'),
+    # path('posts/<int:pk>/', views.PostView.as_view(), name='PostView'),
+    path('', include(router.urls)),
 ]
--- drftutorial/instagram/views.py
+++ drftutorial/instagram/views.py
@@ -1,57 +1,63 @@
 from rest_framework import viewsets, generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from django.shortcuts import get_object_or_404
 from .models import Post
 from .serializers import PostSerializer
 
 # Create your views here.
     
-class PublicPostListView(APIView):
-    def get(self, request):
-        qs = Post.objects.filter(is_public=True)
-        serializer = PostSerializer(qs, many=True)
+# class PublicPostListView(APIView):
+#     def get(self, request):
+#         qs = Post.objects.filter(is_public=True)
+#         serializer = PostSerializer(qs, many=True)
         
-        return Response(
-            serializer.data,
-            status=status.HTTP_200_OK
-        )
+#         return Response(
+#             serializer.data,
+#             status=status.HTTP_200_OK
+#         )
     
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
+#     def post(self, request):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
             
-            return Response(
-                serializer.data, 
-                status=status.HTTP_201_CREATED
-            )
+#             return Response(
+#                 serializer.data, 
+#                 status=status.HTTP_201_CREATED
+#             )
             
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+#         return Response(
+#             serializer.errors,
+#             status=status.HTTP_400_BAD_REQUEST
+#         )
 
 
-class PostView(APIView):
-    def get_object(self, pk):
-        return get_object_or_404(Post, pk=pk)
+# class PostView(APIView):
+#     def get_object(self, pk):
+#         return get_object_or_404(Post, pk=pk)
     
-    def get(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post)
+#     def get(self, request, pk):
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(post)
         
-        return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    def put(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request, pk):
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(post, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def delete(self, request, pk):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, pk):
+#         post = self.get_object(pk)
+#         post.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+# APIView -> mixins -> GenericAPIView -> ViewSet
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
