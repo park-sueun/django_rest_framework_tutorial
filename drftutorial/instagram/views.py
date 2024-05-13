29cd9270ce3b9cc56db96751b46f069bef2bb403
Park, Su Eun <park_sueun@naver.com>
2024-05-13 23:48:18 +0900

    perform_create 활용

--- drftutorial/instagram/views.py
+++ drftutorial/instagram/views.py
@@ -63,6 +63,12 @@ class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
+    def perform_create(self, serializer):
+        # FIXME: 유저 인증이 되어 있다는 가정하에 auth를 지정함
+        author = self.request.user
+        ip = self.request.META['REMOTE_ADDR']
+        serializer.save(author=author, ip=ip)
+    
     @action(detail=False, methods=['GET'])
     def public(self, request):
         qs = self.get_queryset().filter(is_public=True)
