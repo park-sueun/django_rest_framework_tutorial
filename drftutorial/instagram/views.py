c6012e541bfbb99741285c4af67b8f4422de1197
Park, Su Eun <park_sueun@naver.com>
2024-05-15 23:20:53 +0900

    rest_framework.filter의 SearchFilter View에서 활용하기

--- drftutorial/instagram/views.py
+++ drftutorial/instagram/views.py
@@ -4,6 +4,10 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+
+# 별도의 검색엔진을 사용하는 것이 아니라, DBMS의 where절을 사용
+from rest_framework.filters import SearchFilter, OrderingFilter
+
 from django.shortcuts import get_object_or_404
 from .models import Post
 from .serializers import PostSerializer
@@ -63,7 +67,11 @@ from .serializers import PostSerializer
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    
     # permission_classes = [IsAuthenticated] # permission이 적용됨
+    
+    filter_backends = [SearchFilter]
+    search_fields = ['message']     # ?search= -> QuerySet 조건 절에 추가할 필드 지정. 모델 필드 중 문자열 필드만을 지정
     
     def perform_create(self, serializer):
         # FIXME: 유저 인증이 되어 있다는 가정하에 auth를 지정함
